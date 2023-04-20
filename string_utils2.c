cf39f2c695e399b37e4b3735ad07b3c0cb152df1
sindi702 <hamatajsindi@gmail.com>
2023-04-20 00:38:28 -0700

    simple shell

---
+++ string_utils2.c
@@ -0,0 +1,54 @@
+/**
+ * _strlen - find string length
+ * @str: string where to find length
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcat - concatenate 2 strings
+ * @dest: where to add string
+ * @src: string to add
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcpy - copy strings
+ * @dest: where to copy strings
+ * @src: string to copy
+ * Return: coppied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
